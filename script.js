b6917f30c78e0d8e07051ad02276b48f1d36c40a
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2022-08-23 20:05:36 +0500

    Function that will display Time

---
+++ script.js
@@ -0,0 +1,12 @@
+displayTime = () => {
+    let dateTime = new Date();
+    let hrs = dateTime.getHours();
+    let min = dateTime.getMinutes();
+    let sec = dateTime.getSeconds();
+    let session = document.getElementById('session');
+
+    document.getElementById('hours').innerHTML = hrs;
+    document.getElementById('minutes').innerHTML = min;
+    document.getElementById('seconds').innerHTML = sec;
+    document.getElementById('sessions').innerHTML = session;
+};
