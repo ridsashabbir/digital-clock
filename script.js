010b95b7f2545eddbb202b8d0f57ddb18a88b068
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2022-08-23 20:32:52 +0500

    fix format

--- script.js
+++ script.js
@@ -3,11 +3,28 @@ displayTime = () => {
     let hrs = dateTime.getHours();
     let min = dateTime.getMinutes();
     let sec = dateTime.getSeconds();
-    let session = document.getElementById('session');
+    // let session = document.getElementById('session');
 
     document.getElementById('hours').innerHTML = hrs;
     document.getElementById('minutes').innerHTML = min;
     document.getElementById('seconds').innerHTML = sec;
+
+    // set AM and PM accordingly
+
+    if( hrs >= 12){
+        sessions.innerHTML = "PM";
+    }
+    else{
+        sessions.innerHTML = "AM";
+    }
+
+    // change time format
+
+    if(hrs >= 12){
+        hours.innerHTML = hrs - 12;
+    }
+
+
 };
 
 setInterval(displayTime, 1000);
